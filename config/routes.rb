556f2063df9adb6972b2e51ab0bbf3dca7fb2896
Roderick Mendoza II <rodmendoza07@gmail.com>
2023-04-23 20:52:20 -0700

    implemented backend for user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  post 'api/test', to: 'application#test'
+
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: :create
+    resource :session, only: [:show, :create, :destroy]
+  end
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
