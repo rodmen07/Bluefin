f0a884fe207995684857cc5a3f725dcaf3ab2b9f
Roderick Mendoza II <rodmendoza07@gmail.com>
2023-04-23 21:08:01 -0700

    completed initial frontend setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  post 'api/test', to: 'application#test'
+  # post 'api/test', to: 'application#test'
 
   namespace :api, defaults: { format: :json } do
     resources :users, only: :create
