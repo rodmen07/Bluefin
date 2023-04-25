a3f7986efd1f35dfc9f0bf760c34dfb221a667ea
Roderick Mendoza II <rodmendoza07@gmail.com>
2023-04-25 09:02:56 -0700

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,8 +15,8 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
 
-  Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
-  or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
+  # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
+  # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
