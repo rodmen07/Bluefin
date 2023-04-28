ee802b0efbfaeb9b36ba0ede3c4d187e84a2254f
Roderick Mendoza II <rodmendoza07@gmail.com>
2023-04-28 11:32:50 -0700

    fixed ProfileButton Component import

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import ProfileButton from '../../../ProfileButton';
+import ProfileButton from './ProfileButton'
 import LoginFormModal from '../LoginFormModal';
 import SignupFormModal from '../SignupFormModal';
 import './Navigation.css';
