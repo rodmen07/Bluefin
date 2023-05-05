f9405661f07507da09595bba6375fd9dc9d745cc
Chuck-Woody <rodmendoza07@gmail.com>
2023-05-05 09:13:09 -0700

    udpated user profile layout

--- frontend/src/components/Listings/UserListingsIndex.js
+++ frontend/src/components/Listings/UserListingsIndex.js
@@ -4,23 +4,35 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom/cjs/react-router-dom.min';
 import ListingCreateFormModal from './ListingCreateModal';
 import ListingEditFormModal from './ListingEditModal';
-import './UserListingsIndex.css';
 import ListingDeleteFormModal from './ListingsDeleteModal';
+import { useState } from 'react';
+import './UserListingsIndex.css';
 
 function UserBio() {
+  const [showBio, setShowBio] = useState(false);
+
   return (
     <>
-    <h3>User Bio</h3>
-    <img src="https://media.istockphoto.com/id/909675728/photo/businessman.jpg?s=612x612&w=0&k=20&c=AFoV-1P3FanXt4YKc37WsgPIiZvifm90_zDB1ZLVT4c="></img>
-    <div className="user-bio">
-      "Hi there! I'm a real estate enthusiast and a seasoned investor with a passion for finding the perfect home for my clients. With over a decade of experience in the industry, I have a keen eye for detail and a deep understanding of the local market trends. Whether you're looking to buy or sell a home, I'm committed to providing you with the best experience possible. From the initial consultation to the closing, I'll be with you every step of the way to ensure a smooth and stress-free transaction. In my free time, you can find me exploring new neighborhoods and checking out the latest listings. I look forward to working with you and helping you achieve your real estate goals!"
-    </div>
+      <h3>User Bio</h3>
+      <img src="https://media.istockphoto.com/id/909675728/photo/businessman.jpg?s=612x612&w=0&k=20&c=AFoV-1P3FanXt4YKc37WsgPIiZvifm90_zDB1ZLVT4c="></img>
+      <div className="user-bio">
+        {showBio ? (
+          <>
+            <p>
+              "Hi there! I'm a real estate enthusiast and a seasoned investor with a passion for finding the perfect home for my clients. With over a decade of experience in the industry, I have a keen eye for detail and a deep understanding of the local market trends. Whether you're looking to buy or sell a home, I'm committed to providing you with the best experience possible. From the initial consultation to the closing, I'll be with you every step of the way to ensure a smooth and stress-free transaction. In my free time, you can find me exploring new neighborhoods and checking out the latest listings. I look forward to working with you and helping you achieve your real estate goals!"
+            </p>
+            <button onClick={() => setShowBio(false)}>Hide Bio</button>
+          </>
+        ) : (
+          <button onClick={() => setShowBio(true)}>Show Bio</button>
+        )}
+      </div>
     </>
-  )
+  );
 }
 
-function UsersListings() {
 
+function UsersListings() {
   const dispatch = useDispatch();
   const listings = useSelector(getListings);
   const { userid } = useParams();
@@ -33,26 +45,26 @@ function UsersListings() {
 
   return (
     <>
-      <UserBio />
       <ListingCreateFormModal/>
       <div className="listings-container">
         {filteredListings.map(listing => {
-            return (
-              <div key={listing.id} className="listing">
-                <Link to={`/listings/${listing.id}`}>
-                  <img src={listing.photoUrls} alt="listing" className="listingPhoto"/>
-                </Link>
-                <div className="listing-info">
-                  <h2>${listing.price.toLocaleString()}</h2>
-                  <p>{listing.bed} Beds {listing.baths} Baths {listing.sqft.toLocaleString()} Sq. Ft.</p>
-                  <p>{listing.address}</p>
-                  <ListingEditFormModal listingId={listing.id}/>
-                  <ListingDeleteFormModal listingId={listing.id}/>
-                </div>
+          return (
+            <div key={listing.id} className="listing">
+              <Link to={`/listings/${listing.id}`}>
+                <img src={listing.photoUrls} alt="listing" className="listingPhoto"/>
+              </Link>
+              <div className="listing-info">
+                <h2>${listing.price.toLocaleString()}</h2>
+                <p>{listing.bed} Beds {listing.baths} Baths {listing.sqft.toLocaleString()} Sq. Ft.</p>
+                <p>{listing.address}</p>
+                <ListingEditFormModal listingId={listing.id}/>
+                <ListingDeleteFormModal listingId={listing.id}/>
               </div>
+            </div>
             );
-        })}
+          })}
       </div>
+      <UserBio />
     </>
   )
 }
