f02411ffd7c97065a7de1d90e7080768071bd2bd
Roderick Mendoza <rodmendoza07@gmail.com>
2023-05-31 14:15:34 -0700

    added disclaimer

--- frontend/src/components/Listings/ListingShow.js
+++ frontend/src/components/Listings/ListingShow.js
@@ -30,7 +30,7 @@ export default function ListingShow () {
             if (mapElement) {
                 const map = new window.google.maps.Map(mapElement, {
                 center: { lat: randomCity.lat, lng: randomCity.lng },
-                zoom: 18,
+                zoom: 16,
                 });
 
                 const marker = new window.google.maps.Marker({
@@ -53,7 +53,7 @@ export default function ListingShow () {
                         <img src={listing.photoUrls} alt="" className="listing-show-image"/>
                         <div id="map"></div>
                     </div>
-                    <div id="address">{listing?.address}</div>
+                    <div id="address">{listing?.address} (Note: Address and Google Maps locations are randomly generated)</div>
                     <h2>${listing?.price.toLocaleString()} - For Sale &nbsp;&nbsp;&nbsp; {listing.bed} Beds {listing.baths} Baths {listing.sqft.toLocaleString()} Sq. Ft.</h2>
                     <h3>About this home</h3>
                     <p className="description"> Welcome to this stunning home located in the heart of the charming Willow Glen neighborhood! This home is perfect for those who love to entertain. The spacious living room features a cozy fireplace, built-in shelving, and large windows that fill the room with natural light. The chef's kitchen boasts granite countertops, stainless steel appliances, and ample cabinet space. The primary bedroom is a peaceful retreat with a private en-suite bathroom and a walk-in closet. Two additional bedrooms offer plenty of space for family or guests. You'll love spending time outdoors in the beautifully landscaped backyard, which features a large patio area and lush greenery. Conveniently located near top-rated schools, shopping, dining, and entertainment options, this home has everything you need for comfortable living. Don't miss out on this wonderful opportunity to make this house your forever home!</p>
